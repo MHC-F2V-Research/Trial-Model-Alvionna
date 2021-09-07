cbdfd3b531fbdec86c3095c0d5175c851f7311ff
Alvionna Sunaryo <61591621+alvionna@users.noreply.github.com>
2021-09-07 21:10:30 +0700

    methodized the code

--- blurring.py
+++ blurring.py
@@ -3,12 +3,136 @@
  *
  * usage: python GaussBlur.py <filename> <sigma>
 """
+from PIL import Image
 import skimage
 from skimage.viewer import ImageViewer
 import sys
 import cv2
 import numpy as np
+import os
+import random
+import itertools
 from matplotlib import pyplot as plt
+
+
+##############################################
+##############################################
+# METHODS
+##############################################
+##############################################
+
+def resize(directory):
+    for filename in os.listdir(directory):
+        image = cv2.imread(os.path.join(directory, filename))
+        dim = (480,270)
+        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
+        path = "kinova_color_resized_images" #RENAME IT
+        if not os.path.exists(path):
+            os.makedirs(path)
+        cv2.imwrite(os.path.join(path, filename), img_color)
+
+def blurring(directory_color, directory_depth):
+    n_avg1 = random.randint(0,100)
+    n_avg2 = random.randint(0,100)
+    n_gauss1 = random.randint(0,100)
+    n_gauss2 = random.randint(0,100)
+    n_med = random.randint(0,100)
+    n_bil1 = random.randint(0,100)
+    n_bil2 = random.randint(0,100)
+    n_bil3 = random.randint(0,100)
+    float = random.uniform(0.1, 30.0)
+
+    for (file_color, file_depth) in zip (os.listdir(directory_color), os.listdir(directory_depth)):
+        if file_color.endswith(".png") or file_depth.endswith(".png"):
+            img_color = cv2.imread(os.path.join(directory_color, file_color))
+            img_depth =  cv2.imread(os.path.join(directory_depth, file_depth))
+
+            blur_avg_color = cv2.blur(img_color, (n_avg1, n_avg2))
+            blur_avg_depth = cv2.blur(img_depth, (n_avg1, n_avg2))
+
+            blur_gaussian_color = cv2.GaussianBlur(img_color, (n_gauss, n_gauss), float, float)
+            blur_gaussian_depth = cv2.GaussianBlur(img_depth, (n_gauss1, n_gauss2), float, float)
+
+            median_blur = cv2.medianBlur(img_color, n_med)
+            median_blur = cv2.medianBlur(img_depth, n_med)
+
+            bilateral_color = cv2.bilateralFilter(img_color, n_bil1, n_bil2, n_bil3)
+            bilateral_depth = cv2.bilateralFilter(img_depth, n_bil1, n_bil2, n_bil3)
+
+            path_color = "kinova_color_blur_images"
+            if not os.path.exists(path_color):
+                os.makedirs(path)
+            cv2.imwrite(os.path.join(path_color, file_color), img_color)
+
+            path_depth = "kinova_depth_blur_images"
+            if not os.path.exists(path_depth):
+                os.makedirs(path)
+            cv2.imwrite(os.path.join(path_depth, file_depth), img_depth)
+
+
+    # for file_color in os.listdir(directory_color):
+    #     if file_color.endswith(".png"):
+    #         img_color = cv2.imread(os.path.join(directory_color, file_color))
+    #         blur_average = cv2.blur(img_color, (n_avg1, n_avg2))
+    #         blur_gaussian = cv2.GaussianBlur(img_color, (n_gauss, n_gauss), float, float)
+    #
+    #         median_blur = cv2.medianBlur(img_color, n_med)
+    #         bilateral = cv2.bilateralFilter(img_color, n_bil1, n_bil2, n_bil3)
+    #
+    #         path = "kinova_color_blur_images"
+    #         if not os.path.exists(path):
+    #             os.makedirs(path)
+    #         cv2.imwrite(os.path.join(path, file_color), img_color)
+    #     else:
+    #         #do smth
+    #         break
+    # for file_depth in os.listdir(directory_depth):
+    #     if file_depth.endswith(".png"):
+    #         img_depth =  cv2.imread(os.path.join(directory_depth, file_depth))
+    #         blur_average = cv2.blur(img_depth, (n_avg1, n_avg2))
+    #         blur_gaussian = cv2.GaussianBlur(img_depth, (n_gauss1, n_gauss2), float, float)
+    #
+    #         median_blur = cv2.medianBlur(img_depth, n_med)
+    #         bilateral = cv2.bilateralFilter(img_depth, n_bil1, n_bil2, n_bil3)
+    #
+    #         path = "kinova_depth_blur_images"
+    #         if not os.path.exists(path):
+    #             os.makedirs(path)
+    #         cv2.imwrite(os.path.join(path, file_depth), img_depth)
+
+##############################################
+##############################################
+# RESIZING
+##############################################
+##############################################
+
+#######################
+# PIL WAY
+#######################
+
+image = Image.open('kinova_color_images/myframe000320.png')
+new_image = image.resize((480,270))
+new_image.save('resized_myframe000320.png')
+
+#######################
+# OpenCV WAY
+#######################
+
+img = cv2.imread('kinova_color_images/myframe000320.png')
+scale_percent = 220 # percent of original size
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
+##############################################
+##############################################
+# BLURRING
+##############################################
+##############################################
+
 
 #######################
 # SKIMAGE WAY
@@ -34,6 +158,24 @@ blurred_show.show()
 #######################
 
 img = cv2.imread('kinova_color_images/myframe000320.png')
+
+directory = "kinova_color_images"
+
+for filename in os.listdir(directory):
+    if filename.endswith(".png"):
+        n = random.randint(0,100)
+        img = cv2.imread(os.path.join(directory,filename))
+        blur_average = cv2.blur(img, (n,n))
+
+        float = random.uniform(0.1, 30.0)
+        blur_gaussian = cv2.GaussianBlur(img, (n,n), float, float)
+
+        cv2.medianBlur(img, n)
+        cv2.bilateralFilter(img,n,n,n)
+        continue
+    else:
+        #do smth
+        break
 
 ### averaging method -> convolving the image with a normalized box filter
 
@@ -98,3 +240,4 @@ plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(bilateral),plt.title('Bilateral Filtering')
 plt.xticks([]), plt.yticks([])
 plt.show()
+
