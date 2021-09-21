ce3fdec8c9369a0eba3d482d6b3d389433ad6b6e
Alvionna Sunaryo <61591621+alvionna@users.noreply.github.com>
2021-09-21 21:24:37 +0700

    update on remix net.py

--- mock-remixnet.py
+++ mock-remixnet.py
@@ -16,6 +16,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+nsf = 32 #size of siamese feature map
+siamese_batch = 64
 batch_size = 128
 size_z = 100
 ngf = 64
@@ -29,6 +31,33 @@ class View(nn.Module):
 
     def forward(self, x):
         return x.view(*self.shape)
+
+# DON'T FORGET TO NORMALIZE THE IMAGE (normalize((mean,mean,mean), (sd,sd,sd))) for each channel
+
+# the paper accepts a 28x28 pixel grayscale image & the output a vector of length 50
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder,self).__init__()
+        self.main = nn.Sequential(
+            nn.BatchNorm2d(siamese_batch),
+            nn.Conv2d(siamese_batch, nsf, (2,2), stride=0, padding=1, bias=False),
+            nn.Tanh(),
+
+            nn.Conv2d(nsf, nsf/2, (5,5), stride=0, padding=1, bias=False),
+            nn.Linear(nsf/2, nsf * 4, bias=False),
+            nn.ReLu(),
+
+            nn.Linear(nsf * 4, 50, bias = False),
+            nn.ReLU()
+        )
+
+    #using the same architecture but having 2 inputs going through the same encoder
+    def forward(self, input1, input2):
+        # forward pass of input 1
+        output1 = self.main(input1)
+        # forward pass of input 2
+        output2 = self.main(input2)
+        return output1, output2
 
 # def generator_model():
 #     model = Sequential()
@@ -46,7 +75,6 @@ class View(nn.Module):
 #     model.add(Activation('tanh'))
 #     return model
 
-
 class Generator(nn.Module):
     def __init__(self):
         super(Generator,self).__init__()
@@ -55,10 +83,10 @@ class Generator(nn.Module):
             nn.Tanh(),
 
             nn.Linear(ngf * 16, 128 * 7 * 7, bias = False),
-            nn.BatchNorm2d(128 * 7 * 7),
+            nn.BatchNorm2d(batch_size * 7 * 7),
             nn.Tanh(),
 
-            nn.View((7,7,128)), #home-made, there's no reshape in pytorch, and we cannot use view in nn.sequential
+            nn.View((7,7,batch_size)), #home-made, there's no reshape in pytorch, and we cannot use view in nn.sequential
             nn.Upsample((2,2), mode = 'nearest'), #mode = nearest, bilinear, bicubic and trilinear
             #nn.ConvTranspose2d(ngf * 2, ngf * 16, (5,5), 2, 1, bias False),
             nn.Conv2d(ngf * 16, ngf * 2, (5,5), stride=2, padding=1, bias=False),
@@ -69,6 +97,9 @@ class Generator(nn.Module):
             nn.Conv2d(ngf, 1, stride=2, padding=1, bias=False),
             nn.Tanh()
         )
+
+    def forward(self, input):
+        return self.main(input)
 
 # def discriminator_model():
 #     model = Sequential()
@@ -112,3 +143,110 @@ class Discriminator(nn.Module):
 
     def forward(self, input):
         return self.main(input)
+
+encoder = Encoder().cuda()
+generator = Generator().cuda()
+discriminator = Discriminator().cuda()
+
+# DON'T FORGET TO CONCATENATE THE INDIVUDAL OUTPUT OF THE SIAMESE ENCODER
+
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on:
+    """
+
+    def __init__(self, margin=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, x0, x1, y):
+        # euclidian distance
+        diff = x0 - x1
+        dist_sq = torch.sum(torch.pow(diff, 2), 1)
+        dist = torch.sqrt(dist_sq)
+
+        mdist = self.margin - dist
+        dist = torch.clamp(mdist, min=0.0)
+        loss = y * dist_sq + (1 - y) * torch.pow(dist, 2)
+        loss = torch.sum(loss) / 2.0 / x0.size()[0]
+        return loss
+
+learning_rate = 0.0005
+beta_1 = 0.5
+criterion_contrastive = ContrastiveLoss() #can use in-build loss functions, this one is for siamese network
+criterion_triplet = torch.nn.TripletMarginLoss(margin = 0.1, p=2) #p = the norm degree for pairwise distance
+criterion_GD = nn.BCELoss()
+
+#weight decay = L2 penalty
+optimizerG = optim.Adam(generator.parameters(), lr=learning_rate, betas=(beta1, 0.999), weight_decay=0.0005)
+optimizerD = optim.Adam(discriminator.parameters(), lr=learning_rate, betas=(beta1, 0.999), weight_decay=0.0005)
+optimizerE = optim.Adam(encoder.parameters(), lr=learning_rate, betas=(beta1, 0.999), weight_decay=0.0005)
+
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+num_epochs = 1000
+
+
+def train():
+    img_list = []
+    G_losses = []
+    D_losses = []
+    E_losses = []
+    iters = 0
+    num_epochs = 1000
+
+    for epochs in range(num_epochs):
+        for i, data in enumerate(dataset, 0):
+            #encoder training
+            img0, img1 , label = data
+            img0, img1 , label = img0.cuda(), img1.cuda() , label.cuda()
+            optimizerE.zero_grad()
+            output1,output2 = encoder(img0,img1)
+            loss_encoder = criterion_contrastive(output1,output2,label)
+            # loss_triplet  = criterion_triplet(output1, output2,label)
+            loss_encoder.backward()
+            # loss_triplet.backward()
+            optimizerE.step()
+
+            output_encoder = torch.cat((output1, output2), 1)
+
+            #train all-fake
+            optimizerD.zero_grad()
+            fake_sample = generator(output_encoder)
+            fake_label = torch.zeros(fake_sample.size(0), 1, device=device)
+            fake_preds = discriminator(fake_sample)
+            loss_discriminator_fake = criterion_GD(fake_preds, fake_label)
+            loss_discriminator_fake.backward()
+            D_x = fake_preds.mean().item()
+
+            #train all-real
+            real_label = torch.ones(output_encoder.size(0), 1, device=device)
+            real_preds = discriminator(output_encoder) #what is the real image? we can use img0, img1
+            loss_discriminator_real = criterion_GD(real_preds, real_label)
+            loss_discriminator_real.backward()
+            D_G_z1 = real_preds.mean().item()
+
+            discriminator_loss = loss_discriminator_fake + loss_discriminator_fake
+
+            optimizerD.step() #update the discriminator
+
+            #generator training
+
+            optimizerG.zero_grad()
+            gen_label = torch.ones(output_encoder.size(0), 1, device=device)
+            fake_image = generator(output_encoder)
+            #trying to fool the discriminator
+            gen_preds = discriminator(fake_image)
+            loss_generator = criterion_GD(gen_preds, gen_label)
+
+            loss_generator.backward()
+            D_G_z2 = gen_preds.mean().item()
+
+            optimizerG.step()
+
+            G_losses.append(loss_genenator.item())
+            D_losses.append(discriminator_loss.item())
+            E_losses.append(loss_contrastive.item())
+            
+        return G_losses, D_losses, E_losses
